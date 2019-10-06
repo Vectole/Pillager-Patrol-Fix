42469b1bcafa091466b20fd98c4c5fb7a14064d6
Vectole <vectole@protonmail.com>
2019-10-06 16:42:43 +0300

    Fixed distance calculation, improved logging

--- src/main/java/xyz/vectole/projects/pillagerfix/PillagerControl.java
+++ src/main/java/xyz/vectole/projects/pillagerfix/PillagerControl.java
@@ -17,8 +17,8 @@ public class PillagerControl implements Listener {
             for(Entity entity : location.getWorld().getEntities()) {
                 if(entity instanceof Pillager) {
                     if(((Pillager)entity).isPatrolLeader()) {
-                        if(entity.getLocation().distance(location) <= location.getWorld().getViewDistance()*2) {
-                            System.out.println("A pillager spawn has been cancelled!");
+                        if(entity.getLocation().distance(location) <= location.getWorld().getViewDistance()*2*16) {
+                            System.out.println("A pillager spawn has been cancelled! Location: " + location.getBlockX() + " | " + location.getBlockY() + " | " + location.getBlockZ());
                             event.setCancelled(true);
                             return;
                         }
